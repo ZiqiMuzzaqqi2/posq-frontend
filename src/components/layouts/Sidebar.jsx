296ded8569123464e4abb575bdc53005c4464855
ZiqiMuzzaqqi2 <muzzaqqi22@student.polindra.ac.id>
2026-05-22 17:25:17 +0700

    first commit

--- src/components/layouts/Sidebar.jsx
+++ src/components/layouts/Sidebar.jsx
@@ -99,7 +99,7 @@ const Sidebar = ({ isCollapsed, toggleSidebar }) => {
 
         <button
           onClick={handleLogout}
-          className={`w-full flex items-center gap-3 px-3 py-2 rounded-xl text-white/70 hover:text-white hover:bg-white/10 transition-all duration-200
+          className={`w-full flex items-center gap-3 px-3 py-2 rounded-xl text-red-500 hover:text-white hover:bg-red-600 transition-all duration-200
             ${isCollapsed ? "justify-center" : ""}`}
           title={isCollapsed ? "Logout" : ""}
         >
